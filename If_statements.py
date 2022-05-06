7c290e5b90437026b0418c85b18ae5d1814a6f58
PastelKobold <103293322+PastelKobold@users.noreply.github.com>
2022-05-06 13:30:59 -0400

    Saving_if

--- If_statements.py
+++ If_statements.py
@@ -1,7 +1,15 @@
-x = 7
-if x < 10:
-    print('x is smaller than 10!')
-if x > 20:
-    print('x is greater than 20!')
+#Wow I can comment
+User_input = 0
+User_input = input('x = ?: ')
+x = int(User_input)
+print('x =', x)
+while x < 25:
+    if x < 10:
+        print('x is smaller than 10!')
+    if x > 10 | x < 20 :
+        print('x is between 10 and 20!')
+    if x > 20:
+        print('x is greater than 20!')
+    x = x + 1
 
 print('All done!')

d67bb9b4d9bf597d9137256b619d68badcc9617d
PastelKobold <swebster495@gmail.com>
2022-05-11 16:58:01 -0400

    Ha

--- If_statements.py
+++ If_statements.py
@@ -6,9 +6,9 @@ print('x =', x)
 while x < 25:
     if x < 10:
         print('x is smaller than 10!')
-    if x > 10 | x < 20 :
+    elif x > 10 | x < 20 :
         print('x is between 10 and 20!')
-    if x > 20:
+    elif x > 20:
         print('x is greater than 20!')
     x = x + 1
 

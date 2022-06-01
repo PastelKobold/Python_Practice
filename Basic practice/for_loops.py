2a574b6deafaedbb4acdf722811e6dd61c0a9669
PastelKobold <103293322+PastelKobold@users.noreply.github.com>
2022-06-01 16:51:34 -0400

    Organizing files

---
+++ Basic practice/for_loops.py
@@ -0,0 +1,15 @@
+#for loop for strings
+
+group_names = ['Heather' , 'Jason' , 'Ashley']
+for group_name in group_names:
+    print('Member:', group_name)
+
+#for loop for integers
+
+for i in [1,6,9]:
+    if i==1:
+        print(i)
+    if i==6:
+        print('The number is 6')
+    if i==9:
+        print('Program is finished')
